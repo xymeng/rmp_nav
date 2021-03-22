621509273c61c6be83d1059ce5f000245584ec4a
xymeng <xiangyun@cs.washington.edu>
2021-03-22 18:04:10 -0400

    Use pip version of pybind11

--- rmp_nav/common/compile_math_utils.sh
+++ rmp_nav/common/compile_math_utils.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 g++ -O3 -Wall -fPIC -shared -std=c++11 \
-math_utils.cpp \
--o math_utils_cpp.so \
--static-libstdc++ \
--Wl,-rpath,"\$ORIGIN" \
-$@
+    math_utils.cpp \
+    -o math_utils_cpp.so \
+    -static-libstdc++ \
+    -Wl,-rpath,"\$ORIGIN" \
+    $(python3 -m pybind11 --includes) \
+    $@
--- rmp_nav/simulation/compile_map_utils.sh
+++ rmp_nav/simulation/compile_map_utils.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 g++ -O3 -Wall -fPIC -shared -std=c++11 \
-map_utils.cpp \
--o map_utils_cpp.so \
--static-libstdc++ \
--Wl,-rpath,"\$ORIGIN" \
--I /usr/local/include \
--L /usr/local/lib \
-$@
+    map_utils.cpp \
+    -o map_utils_cpp.so \
+    -static-libstdc++ \
+    -Wl,-rpath,"\$ORIGIN" \
+    $(python3 -m pybind11 --includes) \
+    $@
