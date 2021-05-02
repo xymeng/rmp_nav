533811205ff27437568ac9c233b2b44353afebf0
xymeng <xiangyun@uw.edu>
2021-05-02 15:44:35 -0700

    Updated model factory.

--- cbe/model_factory.py
+++ cbe/model_factory.py
@@ -1,9 +1,10 @@
 import os
-from rmp_nav.common.utils import get_model_dir
-from .inference import ProgressTrackerEndToEnd
+from rmp_nav.common.utils import get_model_dir, get_project_root
+from .inference import TrackerCBE, TrackerRPF
 
 
 tracker_models_dir = os.path.join(get_model_dir(), 'cbe')
+experiments_dir = os.path.join(get_project_root(), 'cbe/experiments')
 
 
 _model_dict = {}
@@ -16,8 +17,17 @@ def register(f):
 
 @register
 def cbe():
-    tracker = ProgressTrackerEndToEnd(
+    tracker = TrackerCBE(
         weights_file=os.path.join(tracker_models_dir, 'randfi-fmax64-noodom-normwp-dagger-jitter-v2-cf0-model.5'))
+    return {
+        'tracker': tracker,
+    }
+
+
+@register
+def rpf():
+    tracker = TrackerRPF(
+        weights_file=os.path.join(tracker_models_dir, 'rpf-model.5'))
     return {
         'tracker': tracker,
     }
